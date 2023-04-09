e6a93b377132c9eb4d72a4a8744b67bafb334d7d
HimanshuSingh1407 <singh.himanshu1407@gmail.com>
2023-04-10 01:56:56 +0530

    first commit

--- backend/src/config/connectDatabase.config.js
+++ backend/src/config/connectDatabase.config.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose");
 
-
 const ConnectDatabase = async () => {
   try {
-    await mongoose.connect(
-      process.env.MONGODB-URL
-    );
+    await mongoose.connect(process.env.MONGODB_URL);
   } catch (error) {
     console.log(error.message);
   }
